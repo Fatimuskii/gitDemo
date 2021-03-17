f3cb9101f4a601bc2d4dca4b0a03c247713a5b60
elenaIancu <80821415+elenaIancu@users.noreply.github.com>
2021-03-17 14:27:36 +0100

    controller de Salomon

--- project/src/main/java/com/formacion/project/Controller.java
+++ project/src/main/java/com/formacion/project/Controller.java
@@ -2,6 +2,6 @@ package com.formacion.project;
 
 public class Controller {
 
-	Productos prod= new Productos(1,"producto1");
-	prod = new Productos(2, "El chunguillo");
+	Productos prod= new Productos(1 ,"producto1");
+	Productos prod2 = new Productos(2, "El chunguillo");
 }
