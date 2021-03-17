4f97fa2515c6b87c3b3059c46bda175e5953611d
elenaIancu <80821415+elenaIancu@users.noreply.github.com>
2021-03-17 13:41:37 +0100

    peroductos añadidos desde controller

--- project/src/main/java/com/formacion/project/Controller.java
+++ project/src/main/java/com/formacion/project/Controller.java
@@ -3,5 +3,10 @@ package com.formacion.project;
 public class Controller {
 
 	Productos prod= new Productos(1,"producto1");
+	
+	//crear nuevos productos
+	Productos prod2= new Productos(2,"producto2");
+	Productos prod3= new Productos(3,"producto3");
+	
 
 }
